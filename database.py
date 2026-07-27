989fc2693c2ea864a71c0f9f287488a9e82db3d8
seoriny022 <seorin2022@gmail.com>
2026-07-27 10:12:11 +0900

    add backend

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = "sqlite:///./ansim.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
