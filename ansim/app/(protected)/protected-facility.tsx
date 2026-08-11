e748dff9cc057c0bc5475991a1b60bbdd40982c5
yubeen217 <yubeen217@gmail.com>
2026-08-12 06:12:34 +0900

    구조 재구성

---
+++ ansim/app/(protected)/protected-facility.tsx
@@ -0,0 +1,5 @@
+import FacilityListScreen from '@/components/institutions/facility-list-screen';
+
+export default function ProtectedFacilityScreen() {
+  return <FacilityListScreen variant="protected" returnRoute="/protected-main" />;
+}
---
+++ ansim/features/contacts/protector-contact-store.ts
@@ -0,0 +1,9 @@
+let protectorPhone = '01012345678';
+
+export function getProtectorPhone() {
+  return protectorPhone;
+}
+
+export function setProtectorPhone(phone: string) {
+  protectorPhone = phone;
+}
