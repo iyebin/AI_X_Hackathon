cece764eef84470f2a27632f4ffa15725f30674a
iyebin <yebin061819@gmail.com>
2026-08-12 22:32:42 +0900

    db 연결

--- backend/database.py
+++ backend/database.py
@@ -7,11 +7,11 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./ansim.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres.zuubggonagdhdkphxzrq:ansimhackathon@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres")
 
 # Render/Supabase에서 postgres:// 형태를 제공하는 경우 SQLAlchemy 형식으로 변환
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+# if DATABASE_URL.startswith("postgres://"):
+#     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 connect_args = {}
 if DATABASE_URL.startswith("sqlite"):
